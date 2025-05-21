3ced91379dd2b430a03b3f9d14b8929a37915b53
MULTISOFTOFFICE/ma.vesenkov <saintbidies@gmail.com>
2025-05-21 17:42:05 +0300

    modified build_gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.21")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
